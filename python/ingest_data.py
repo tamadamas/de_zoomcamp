82c116378ef615efcfb48e6fb92049a6a25e366a
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-12-13 14:56:22 +0100

    Refactor to use common docker-services in python and rust modules

--- python/ingest_data.py
+++ python/ingest_data.py
@@ -13,18 +13,25 @@ def main() -> None:
     lf: LazyFrame = pl.scan_parquet("../data/yellow_tripdata_2025-01.parquet")
     print(lf.head())
 
+    count = lf.select(pl.len()).collect(engine="streaming").item()
+    current_count = 0
+    batch_size = 100_000
+
     # Replace table for the first time and then append
     mode: str = "replace"
 
-    for df in lf.collect_batches(chunk_size=100_000):
+    for df in lf.collect_batches(chunk_size=batch_size):
+        print(f"Processing batch {current_count}/{count}")
+
         df.write_database(
-            table_name="trips",
+            table_name="yellow_tripdata",
             connection=database_url,
             if_table_exists=mode,
             engine="adbc",
         )
 
         mode = "append"
+        current_count += batch_size
 
 
 if __name__ == "__main__":
